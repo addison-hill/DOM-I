f83ec8786510b582e4bc56505ac3abd85552095f
Addison Hill <adddison.hill@gmail.com>
2019-09-30 17:10:33 -0500

    Complete MVP

--- js/index.js
+++ js/index.js
@@ -40,3 +40,78 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// --------------Start my work ------------------------
+
+const Links = document.getElementsByTagName('a');
+Array.from(Links).forEach((link) => {
+  link.textContent = "Services";
+})
+
+Links[1].textContent = "Product";
+Links[2].textContent = "Vision";
+Links[3].textContent = "Features";
+Links[4].textContent = "About";
+Links[5].textContent = "Contact";
+
+//------------------ CTA ---------------------------------
+
+const title = document.querySelector('h1');
+title.textContent = 'DOM Is Awesome';
+
+const button = document.querySelector('button');
+button.textContent = "Get Started";
+
+const image = document.getElementById('cta-img');
+image.src = "img/header-img.png";
+
+//----------------- Top-Content --------------------------
+
+const topH4s = document.querySelectorAll('.top-content h4');
+Array.from(topH4s).forEach((element) => {
+  element.textContent = "Features";
+})
+topH4s[1].textContent = "About";
+
+
+const topPara = document.querySelectorAll('.top-content p');
+Array.from(topPara).forEach((element) => {
+  element.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+})
+topPara[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+const midImage = document.getElementById('middle-img');
+midImage.src = "img/mid-page-accent.jpg";
+
+//---------------- Bottom-Content -----------------------------
+
+const bottomH4s = document.querySelectorAll('.bottom-content h4');
+Array.from(bottomH4s).forEach((element) => {
+  element.textContent = "Services";
+})
+bottomH4s[1].textContent = "Product";
+bottomH4s[2].textContent = "Vision";
+
+const bottomPara = document.querySelectorAll('.bottom-content p');
+Array.from(bottomPara).forEach((element) => {
+  element.textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+})
+bottomPara[1].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+bottomPara[2].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+//------------------------- Contact -----------------------------
+
+const contactH4 = document.querySelector('.contact h4');
+contactH4.textContent = "Contact";
+
+const contactPara = document.querySelectorAll('.contact p');
+Array.from(contactPara).forEach((element) => {
+  element.textContent = "123 Way 456 Street Somewhere, USA";
+})
+contactPara[1].textContent = "1 (888) 888-8888";
+contactPara[2].textContent = "sales@greatidea.io";
+
+//----------------------------- Footer ------------------------------
+
+const footer = document.querySelector('footer p');
+footer.textContent = "Copyright Great Idea! 2018";
